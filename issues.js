f9883345417725b80166c87fe4c53225a23d1353
Maria Grazia Alastra <maria@duckduckgo.com>
2016-11-17 16:50:16 -0500

    tweak styles

--- issues.js
+++ issues.js
@@ -4,21 +4,21 @@ this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};
 this["Handlebars"]["templates"]["issues"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression, alias5=container.lambda;
 
-  return "      <li class=\""
+  return "            <li class=\""
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.labels : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + " clearfix\">\n	<span>\n	  <a class=\"tx-clr--dk one-line issue-name\" href=\"https://github.com/duckduckgo/zeroclickinfo-"
+    + " clearfix\">\n	            <div>\n	                <a class=\"tx-clr--dk one-line issue-name\" href=\"https://github.com/duckduckgo/zeroclickinfo-"
     + alias4(((helper = (helper = helpers.repo || (depth0 != null ? depth0.repo : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"repo","hash":{},"data":data}) : helper)))
     + "/issues/"
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\">\n            "
+    + "\">\n                        "
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "\n          </a>\n	  <span class=\"tx-clr--lt tx-size--small\">\n            opened "
+    + "\n                    </a>\n	                <span class=\"tx-clr--lt tx-size--small\">\n                        opened "
     + alias4((helpers.timeago || (depth0 && depth0.timeago) || alias2).call(alias1,(depth0 != null ? depth0.created_at : depth0),false,{"name":"timeago","hash":{},"data":data}))
     + " ago by <a class=\"tx-clr--lt\" href=\"https://github.com/"
     + alias4(alias5(((stack1 = (depth0 != null ? depth0.user : depth0)) != null ? stack1.login : stack1), depth0))
     + "\">"
     + alias4(alias5(((stack1 = (depth0 != null ? depth0.user : depth0)) != null ? stack1.login : stack1), depth0))
-    + "</a> \n	  </span>\n	</span>\n      </li>\n";
+    + "</a> \n	                </span>\n	            </div>\n            </li>\n";
 },"2":function(container,depth0,helpers,partials,data) {
     return "tag-"
     + container.escapeExpression((helpers.slug || (depth0 && depth0.slug) || helpers.helperMissing).call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.name : depth0),{"name":"slug","hash":{},"data":data}))
@@ -26,9 +26,9 @@ this["Handlebars"]["templates"]["issues"] = Handlebars.template({"1":function(co
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "\n<div class=\"issues-list\">\n  <section>\n    <div class=\"ddh-hero blk--hero\">\n      <span class=\"ddh-hero__title\"><strong>List of Open Issues</strong></span>\n    </div>\n    <ul>\n"
+  return "\n<div class=\"issues-list\">\n  <section>\n    <div class=\"whole text-center ddh-hero blk--hero\">\n      <span class=\"ddh-hero__title\"><strong>List of Open Issues</strong></span>\n    </div>\n    <div class=\"blk--content\">\n        <ul>\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    </ul>\n  </section>\n</div>\n\n";
+    + "        </ul>\n    </div>\n  </section>\n</div>\n\n";
 },"useData":true});
 // Strip non-alphanumeric chars from a string and transform it to lowercase
 Handlebars.registerHelper('slug', function(txt) {
