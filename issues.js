054eea0dc26ac9461624fcfe5212da0c7be23c01
Maria Grazia Alastra <maria@duckduckgo.com>
2016-11-17 21:05:29 -0500

    only fetch programming mission issues

--- issues.js
+++ issues.js
@@ -85,7 +85,7 @@ Handlebars.registerHelper("format_time", function(date) {
 
 
 $(document).ready(function() {
-    var url = "https://api.github.com/search/issues?q=repo%3Aduckduckgo%2Fzeroclickinfo-spice+repo%3Aduckduckgo%2Fzeroclickinfo-fathead+repo%3Aduckduckgo%2Fzeroclickinfo-goodies+repo%3Aduckduckgo%2Fzeroclickinfo-longtail+is%3Aissue+is%3Aopen+no:assignee&type=Issues&ref=searchresults&per_page=500";
+    var url = "https://api.github.com/search/issues?q=repo%3Aduckduckgo%2Fzeroclickinfo-spice+repo%3Aduckduckgo%2Fzeroclickinfo-fathead+repo%3Aduckduckgo%2Fzeroclickinfo-goodies+repo%3Aduckduckgo%2Fzeroclickinfo-longtail+is%3Aissue+is%3Aopen+no:assignee+label%3A'Mission%3A+Programming'&type=Issues&ref=searchresults&per_page=500";
 
     $.getJSON(url, function(data) {
         console.log(data); //debug
